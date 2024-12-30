8807b352fa715b65837d659833e27e6ae77a5d99
samgajje <samgajje@gmail.com>
2024-12-30 18:09:16 +0530

    Added keypair to the main.tf

--- main.tf
+++ main.tf
@@ -21,11 +21,20 @@ variable "subnet_id"{
 type = "string"
 }
 
+variable "first"{
+type = "string"
+name = "first"
+}
+
 
 # Creating aws instance
 
 resource "aws_instance" "first_terraform"{
 ami = "vars.ami"
 instance_type = "vars.instance_type"
+key_value = "vars.first"
 subnet_d = "vars.subnet_id"
+tags{
+name = "first_terraform"
+}
 }
